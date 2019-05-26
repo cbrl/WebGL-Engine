bed597f3bbc6c0d14ed2aade2c6b532bd6960fef
Connor <cbroyles94@gmail.com>
2019-05-25 21:34:46 -0400

    Added a main loop to the engine

--- src/engine.ts
+++ src/engine.ts
@@ -2,8 +2,9 @@ import { RenderingMgr } from "./rendering_mgr";
 import { Scene, TestScene } from "./scene";
 
 export class Engine {
-	static instance: Engine = new Engine();
+	private static _instance: Engine = new Engine();
 
+	_running: boolean;
 	_canvas: HTMLCanvasElement;
 	_rendering_mgr: RenderingMgr;
 	_scene: Scene;
@@ -11,30 +12,50 @@ export class Engine {
 	constructor() {
 		this._canvas = <HTMLCanvasElement>document.getElementById("gl_canvas");
 		this._rendering_mgr = new RenderingMgr(this._canvas);
-
+		
 		var button: HTMLElement = document.createElement("button");
 		document.body.appendChild(button);
 		button.textContent = "Load Scene";
 		button.addEventListener("click", () => {
 			Engine.setScene(new TestScene());
-			this.render();
+			Engine.run();
 		});
 	}
 
-	static setScene(scene: Scene): void {
-		Engine.instance._scene = scene;
-		Engine.instance._scene.load();
-	}
-
-	private render(): void {
-		this._rendering_mgr.render(this._scene);
-	}
-
 	static get rendering_mgr(): RenderingMgr {
-		return Engine.instance._rendering_mgr;
+		return Engine._instance._rendering_mgr;
 	}
 
 	static get scene(): Scene {
-		return Engine.instance._scene;
+		return Engine._instance._scene;
+	}
+
+	static setScene(scene: Scene): void {
+		Engine._instance._scene = scene;
+		Engine._instance._scene.load();
+	}
+
+	static run(): void {
+		Engine._instance._running = true;
+		window.requestAnimationFrame(Engine._instance.update.bind(Engine._instance));
+	}
+
+	static stop(): void {
+		Engine._instance._running = false;
+	}
+
+	private update(): void {
+		if (this._scene)
+			this._scene.update();
+
+		this.render();
+
+		if (this._running)
+			window.requestAnimationFrame(this.update.bind(this));
+	}
+
+	private render(): void {
+		if (this._scene)
+			this._rendering_mgr.render(this._scene);
 	}
 }
