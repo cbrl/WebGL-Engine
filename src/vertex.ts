4f49d5a6241e66d94986846d1a3d67555e01f2cd
Connor <cbroyles94@gmail.com>
2019-06-04 22:45:29 -0400

    Changed Vertex abstract class to an interface

--- src/vertex.ts
+++ src/vertex.ts
@@ -22,43 +22,58 @@ class VertexDesc {
 	}
 }
 
-abstract class Vertex {
-	static vertex_descs: VertexDesc[] = [];
+interface Vertex {
+	[key: string]: any;
+	vertex_descs: VertexDesc[];
 }
 
-class VertexPosition extends Vertex {
-	public static vertex_descs: VertexDesc[] = [
+class VertexPosition implements Vertex {
+	private static _vertex_descs: VertexDesc[] = [
 		new VertexDesc("a_position", 3, WebGLRenderingContext.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 0),
 	];
 
-	public position: [number, number, number];
+	position: [number, number, number];
 
 	constructor(position: [number, number, number]) {
-		super();
 		this.position = position;
+	}
+
+	static get vertex_descs(): VertexDesc[] {
+		return VertexPosition._vertex_descs;
+	}
+
+	get vertex_descs(): VertexDesc[] {
+		return VertexPosition._vertex_descs;
 	}
 }
 
-class VertexPositionColor extends Vertex {
-	public static vertex_descs: VertexDesc[] = [
+class VertexPositionColor implements Vertex {
+	private static _vertex_descs: VertexDesc[] = [
 		new VertexDesc("a_position", 3, WebGLRenderingContext.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 0),
 		new VertexDesc("a_color",    3, WebGLRenderingContext.FLOAT, false, 6*Float32Array.BYTES_PER_ELEMENT, 3*Float32Array.BYTES_PER_ELEMENT),
 	];
 
-	public position: [number, number, number];
-	public color: [number, number, number];
+	position: [number, number, number];
+	color: [number, number, number];
 
 	constructor(position: [number, number, number], color: [number, number, number]) {
-		super();
 		this.position = position;
 		this.color = color;
+	}
+
+	static get vertex_descs(): VertexDesc[] {
+		return VertexPositionColor._vertex_descs;
+	}
+
+	get vertex_descs(): VertexDesc[] {
+		return VertexPositionColor._vertex_descs;
 	}
 }
 
 
 function VertexArrayToFloat32Array(vertex_array: Vertex[]): Float32Array {
 	var data: number[] = [];
-	vertex_array.forEach(vertex => {
+	vertex_array.forEach((vertex: Vertex) => {
 		//data.push(...Object.values(vertex).flat());
 		const values: number[][] = Object.keys(vertex).map((key: string) => vertex[key]);
 		values.forEach((value: number[]) => {
